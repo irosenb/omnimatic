a5135aede4369d50e2b0e0e1a93ff608ef00c1fc
Isaac Rosenberg <irosenb7@gmail.com>
2014-11-11 11:38:18 -0800

    no more namedbase

--- lib/generators/omnimatic/install/install_generator.rb
+++ lib/generators/omnimatic/install/install_generator.rb
@@ -1,6 +1,6 @@
 module Omnimatic
   module Generators
-    class InstallGenerator < Rails::Generators::NamedBase
+    class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
       desc "Creates stuff"
       argument :omniauth, :type => :string
